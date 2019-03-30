746891c8ce4e75de526813ed4c28987f81e54dea
Manvir Singh <730069@student.tor.loc>
2019-03-29 20:10:16 -0400

    hhh

---
+++ 730069/730069/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _730069
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld hw = new HelloWorld();
+        }
+    }
+    class HelloWorld
+    {
+        public void hello()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
